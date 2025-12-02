df979a12d457ccf309e4385b312ebd73cbf8b210
Andrew Toderian <atoderian@outlook.com>
2025-12-02 12:38:37 -0800

    Added patient class to make observers individualised to patients

---
+++ HealthMonitoringSystem/src/Patient.java
@@ -0,0 +1,38 @@
+public class Patient {
+    SensorData sensorData;
+    CurrentHeartRateObserver currentHeartRateObserver;
+    CurrentTempObserver currentTempObserver;
+    CurrentAirFlowObserver currentAirFlowObserver;
+
+    String FirstName;
+    String LastName;
+    String PatientID;
+
+    public Patient(String FirstName, String LastName, String PatientID){
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.PatientID = PatientID;
+        sensorData = new SensorData();
+        currentHeartRateObserver = new CurrentHeartRateObserver(sensorData);
+        currentTempObserver = new CurrentTempObserver(sensorData);
+        currentAirFlowObserver = new CurrentAirFlowObserver(sensorData);
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public String getPatientID() {
+        return PatientID;
+    }
+
+    public void setMeasurments(int heartRate, int temperature, int airFlow){
+        sensorData.setMeasurment(heartRate, temperature, airFlow);
+    }
+
+    public void DisplayPatientInfo(){
+        System.out.println(FirstName+" "+LastName+"\n"+PatientID);
+    }
+}
--- HealthMonitoringSystem/src/main.java
+++ HealthMonitoringSystem/src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
+        /*
         SensorData sensorData = new SensorData();
 
         CurrentHeartRateObserver currentHeartRateObserver = new CurrentHeartRateObserver(sensorData);
@@ -8,6 +9,14 @@ public class main {
 
         for(int i =90; i <105;i++){
             sensorData.setMeasurment(i,37,6);
+        }
+
+         */
+        Patient patient1 = new Patient("John","Doe","123");
+
+        patient1.DisplayPatientInfo();
+        for(int i =90; i <105;i++){
+            patient1.setMeasurments(i,30,1);
         }
     }
 }
